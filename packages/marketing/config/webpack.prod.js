eeacab09682504227e6899450e76fb10af11a5f8
edwins-leonardi <edwins.leonardi@gmail.com>
2026-08-15 12:34:18 +0100

    adding github actions

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,24 +1,24 @@
-// const merge = require('webpack-merge');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const commonConfig = require("./webpack.common");
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
-// const devConfig = {
-//     mode: 'development',
-//     devServer: {
-//         port: 9081,
-//         historyApiFallback: {
-//             index: 'index.html'
-//         }
-//     },
-//     plugins: [
-//         new HtmlWebpackPlugin({
-//             template: './public/index.html'
-//         })
-//     ]
-// };
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap'
+            },
+            shared: packageJson.dependencies,
+        }),
+    ]
+};
 
-// module.exports = merge(commonConfig, devConfig);
-
-// module.exports = {
-
-// };
+module.exports = merge(commonConfig, prodConfig);
